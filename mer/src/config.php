c2137d755d1eeb67744d3bf3906eb9d98fadda36
antonio-torres-castro <antonio.jose.torres.castro@gmail.com>
2025-11-20 10:01:19 -0300

    defino BD localmente

--- mer/src/config.php
+++ mer/src/config.php
@@ -2,9 +2,9 @@
 declare(strict_types=1);
 const DB_HOST = '127.0.0.1';
 const DB_PORT = 3306;
-const DB_NAME = 'catalog_db';
-const DB_USER = 'root';
-const DB_PASS = '';
+const DB_NAME = 'comerci3_bdmer';
+const DB_USER = 'comerci3_admin';
+const DB_PASS = 'Micomercial.1';
 const NOTIFY_EMAIL_ENABLED = false;
 const MAIL_FROM = 'no-reply@example.com';
 const COMMISSION_VAT_PERCENT = 19.0;
